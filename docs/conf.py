2c06a2749ee73a700fc0a7d99e0983925cbcd5c4
zmoon <zmoon92@gmail.com>
2022-01-05 11:26:17 -0700

    Rename

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 project = "FortranTip"
 author = "Beliavsky and FortranTip contributors"
-copyright = "2021"
+copyright = f"2021, {author} (website by zmoon)"
 
 extensions = [
     "myst_parser",
@@ -8,11 +8,11 @@ extensions = [
     "sphinx_design",
 ]
 
-html_title = "FortranTip browser"
+html_title = "FortranTip Browser"
 html_theme = "sphinx_book_theme"
 html_theme_options = {
     "use_repository_button": True,
-    "repository_url": "https://github.com/zmoon/FortranTip-site",
+    "repository_url": "https://github.com/zmoon/FortranTipBrowser",
     "repository_branch": "main",
 }
 
--- fortrantip.py
+++ fortrantip.py
@@ -20,7 +20,7 @@ with open("data.yaml", "r") as f:
 
 
 gb_url_fmt = "https://godbolt.org/#g:!((g:!((g:!((g:!((h:codeEditor,i:(filename:'1',fontScale:14,fontUsePx:'0',j:1,lang:fortran,selection:(endColumn:1,endLineNumber:{nl},positionColumn:1,positionLineNumber:{nl},selectionStartColumn:1,selectionStartLineNumber:{nl},startColumn:1,startLineNumber:{nl}),source:'{source:s}'),l:'5',n:'0',o:'Fortran+source+%231',t:'0')),k:50,l:'4',n:'0',o:'',s:0,t:'0'),(g:!((h:compiler,i:(compiler:gfortran112,filters:(b:'0',binary:'1',commentOnly:'0',demangle:'0',directives:'0',execute:'0',intel:'0',libraryCode:'0',trim:'1'),flagsViewOpen:'1',fontScale:14,fontUsePx:'0',j:1,lang:fortran,libs:!(),options:'',selection:(endColumn:1,endLineNumber:1,positionColumn:1,positionLineNumber:1,selectionStartColumn:1,selectionStartLineNumber:1,startColumn:1,startLineNumber:1),source:1,tree:'1'),l:'5',n:'0',o:'x86-64+gfortran+11.2+(Fortran,+Editor+%231,+Compiler+%231)',t:'0')),k:50,l:'4',n:'0',o:'',s:0,t:'0')),l:'2',m:62.300683371298405,n:'0',o:'',t:'0'),(g:!((h:output,i:(compiler:1,editor:1,fontScale:14,fontUsePx:'0',tree:'1',wrap:'1'),l:'5',n:'0',o:'Output+of+x86-64+gfortran+11.2+(Compiler+%231)',t:'0')),header:(),l:'4',m:37.699316628701595,n:'0',o:'',s:0,t:'0')),l:'3',n:'0',o:'',t:'0')),version:4"
-gh_link_fmt = '<a href="https://github.com/zmoon/FortranTip-site/blob/main/src/{fn}" target="_blank"><i class="fab fa-github"></i></a>'
+gh_link_fmt = '<a href="https://github.com/zmoon/FortranTipBrowser/blob/main/src/{fn}" target="_blank"><i class="fab fa-github"></i></a>'
 
 def fortran_to_myst(fn: str) -> str:
     gh = gh_link_fmt.format(fn=fn)
