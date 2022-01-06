b8b04d108d0e2e4989cb2d3268a35f438b5681b7
zmoon <zmoon92@gmail.com>
2022-01-06 08:28:15 -0700

    Fix sum-dim tip comments

--- src/sum-dim.f90
+++ src/sum-dim.f90
@@ -1,10 +1,12 @@
-integer :: v(2,3) ! 2x3 matrix
+integer :: v(2,3)  ! 2x3 matrix
 
 v(1,:) = [1, 2, 3]
 v(2,:) = 10*v(1,:)  ! [10, 20, 30]
 
-print *, sum(v)  ! all elements: 66
-print *, sum(v,dim=1)  ! sum each row: 6 60
-print *, sum(v,dim=2)  ! sum each col: 11 22 33
+print *, v  ! elements in column-major order
+
+print *, sum(v)         ! sum all elements: 66
+print *, sum(v, dim=1)  ! sum each col (along row dim): 11 22 33
+print *, sum(v, dim=2)  ! sum each row (along col dim): 6 60
 
 end
