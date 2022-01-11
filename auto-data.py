ceca1c70f01b6fee21ede65bff2a616e562d866c
zmoon <zmoon92@gmail.com>
2022-01-10 20:06:54 -0700

    Initial parsing of FT readme

---
+++ auto-data.py
@@ -0,0 +1,67 @@
+"""
+Get tip info from the FortranTip readme
+"""
+import datetime
+import re
+from dataclasses import dataclass
+
+import requests
+from dateutil.parser import parse
+
+
+re_readme_line = re.compile(
+    r"\[(?P<title>.+)\]\(https://twitter.com/fortrantip/status/(?P<tweet_id>[0-9]+)\)"
+    r" (?P<time>[0-9]{1,2}:[0-9]{2} [A|P]M)"
+    r" \u00B7 (?P<date>[A-Z][a-z]{2} [0-9]{1,2}, [0-9]{4})"
+    r"(?P<file_links> *\[.*\]\(.+\))*"
+    r"(<br>)?$"
+)
+
+re_link = re.compile(r"\[(?P<text>.*)\]\((?P<target>.+)\)")
+
+
+@dataclass(frozen=True)
+class link:
+    text: str
+    target: str
+
+    def as_md(self) -> str:
+        return f"[{self.text}]({self.target})"
+
+    def check(self):
+        raise NotImplementedError
+
+
+r = requests.get(
+    "https://raw.githubusercontent.com/Beliavsky/FortranTip/main/README.md"
+)
+r.raise_for_status()
+readme = r.text
+
+lines = readme.splitlines()
+ihead = 3
+assert lines[ihead].startswith("### Index")
+
+for line in lines[ihead+1:]:
+    if not line.strip():
+        continue
+
+    m = re_readme_line.match(line)
+    if m is None:
+        print(f"doesn't match the format:\n  {line}")
+
+    else:
+        d = m.groupdict()
+        title = d["title"]
+        dt = parse(f"{d['time']} {d['date']}")
+        s_file_links = d["file_links"]
+        if s_file_links is not None:
+            file_links = [link(a, b) for a, b in re_link.findall(s_file_links)]
+        else:
+            file_links = []
+
+        # Validate finding of file links
+        if not file_links:
+              s_ = line[m.span(2)[1]:]
+              assert s_.count(".f90") == s_.count(".txt") == 0, line
+
